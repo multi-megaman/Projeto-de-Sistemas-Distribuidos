8890abf79ce39933266ccbc3429cc8ad1b61ff0a
Joyce Mirelle <joycemirelleos.02@gmail.com>
2023-08-22 16:03:26 -0300

    CRUD User

--- Backend/API_User_Module/api.ts
+++ Backend/API_User_Module/api.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import {createUser} from './database'
+import {updateUser} from './database'
+import {deleteUser} from './database'
+import {readUser} from './database'
+import {readFeed} from './database'
+import {addFeed} from './database'
 const app = express();
 const PORT = 3000;
 
@@ -15,9 +20,36 @@ app.get('/', (req, res) => {
   res.send('User Module API');
 });
 
+//CRUD USER
 app.post('/createUser', (req, res) => {
   createUser(req, res);
   console.log("Usuario " + req.body.name + " criado com sucesso!");
+});
+
+app.get('/user/:id', (req, res) => {
+  readUser(req, res);
+  console.log("Usuario " + req.body.name + " lido com sucesso!");
+});
+
+app.put('/user/:id/edit', (req, res) => {
+  updateUser(req, res);
+  console.log("Usuario " + req.body.name + " editado com sucesso!");
+});
+
+app.delete('/user/:id/delete', (req, res) => {
+  deleteUser(req, res);
+  console.log("Usuario " + req.body.name + " deletado com sucesso!");
+});
+
+//CRUD FEED
+app.get('/user/:id/feed', (req, res) => {
+  readFeed(req, res);
+  console.log("Feed " + req.body.name + " recuperado com sucesso!");
+});
+
+app.get('/user/:id/add', (req, res) => {
+  addFeed(req, res);
+  console.log("Feed " + req.body.name + " recuperado com sucesso!");
 });
 
 app.listen(PORT, () => {
--- Backend/API_User_Module/database.ts
+++ Backend/API_User_Module/database.ts
@@ -15,3 +15,62 @@ export async function createUser(req: any, res: any) {
     }).then((u) => {res.json(user)
     }).catch((error) => {res.json(error)})
 }
+
+export async function updateUser(req: any, res: any) {
+    const id = req.params.id
+    const { name, email, password } = req.body
+
+    const updatedUser = await prisma.user.update({
+        where: {
+            id: parseInt(id)
+        },
+        data: {
+            name: name,
+            email: email,
+            password: password,
+        },
+    }).then((u) => {res.json(u)
+    }).catch((error) => {res.json(error)})
+}
+
+export async function deleteUser(req: any, res: any) {
+    const id = req.params.id
+    const deletedUser = await prisma.user.delete({
+        where: {
+            id: parseInt(id)
+        }
+    }).then((u) => {res.json(u)
+    }).catch((error) => {res.json(error)})
+}
+
+export async function readUser(req: any, res: any) {
+    const id = req.params.id
+    const readUser = await prisma.user.findFirst({
+        where: {
+            id: parseInt(id)
+        }
+    }).then((u) => {res.json(u)
+    }).catch((error) => {res.json(error)})
+}
+
+export async function readFeed(req: any, res: any) {
+    const id = req.params.id
+    const feed = await prisma.usersLinks.findMany({
+        where: {
+            userId: parseInt(id)
+        }
+    }).then((u) => {res.json(u)
+    }).catch((error) => {res.json(error)})
+}
+
+export async function addFeed(req: any, res: any) {
+    const { id } = req.query
+    const { url } = req.body
+
+    const link = await prisma.link.create({
+        data: {
+            url: url
+        },
+    }).then((u) => {res.json(u)
+    }).catch((error) => {res.json(error)})
+}
